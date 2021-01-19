c256c5ac1768102a650a0379f4c3fd37ab2b8826
profrog <ache159@naver.com>
2021-01-20 06:06:19 +0900

    mingyu kim

--- app/src/main/java/com/example/teamproject01/chat.java
+++ app/src/main/java/com/example/teamproject01/chat.java
@@ -89,16 +89,12 @@ public class chat extends AppCompatActivity{
 
         scroll.post(new Runnable() {
             @Override
-            public void run() {
-                scroll.fullScroll(ScrollView.FOCUS_DOWN);
+            public void run() {scroll.fullScroll(ScrollView.FOCUS_DOWN);
+
             }
         });
 
 
     }
-
-
-
-
 
 }
